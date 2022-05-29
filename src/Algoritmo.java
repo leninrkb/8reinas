c5b7be3e854f44bc9060db1f538b26c36683e741
leninrkb <rkb.lenin@gmail.com>
2022-05-29 00:25:14 -0500

    comprobacion ataques de reina
    control para comprobar a cuantas reinas puede atcar la actual en sus diagonales,  hacia arriba-abajo e izquierda-derecha

--- src/Algoritmo.java
+++ src/Algoritmo.java
@@ -21,6 +21,14 @@ public class Algoritmo {
                                 0,0,0,0,0,0,0,0,
                                 1,0,0,0,0,0,0,1,
                                 0,0,0,0,0,0,0,0,};
+        // int individuo2 [] = {   0,0,0,0,0,1,0,0,
+        //                         0,0,1,0,0,0,0,0,
+        //                         0,0,0,0,0,0,0,0,
+        //                         0,0,0,0,1,1,1,0,
+        //                         0,1,0,0,0,0,0,0,
+        //                         0,0,0,0,0,0,0,0,
+        //                         1,0,0,0,0,1,0,0,
+        //                         0,0,0,0,0,0,0,0,};
         poblacion.add(individuo1);
         poblacion.add(individuo2);
 
@@ -28,10 +36,12 @@ public class Algoritmo {
     }
 
     public void funcionDeAptitud(int[] individuo, int dimensionTablero) {
-        int [] posiciones = determinarPosicionesReinas(individuo);
-        for (int i : posiciones) {
-            System.out.println(i);
-        }
+        List<int[]> segmentos = obtenerSegmentos(individuo, dimensionTablero);
+        int[] posiciones = determinarPosicionesReinas(individuo);
+        // for (int posicionReina : posiciones) {
+        //     determinarNumeroAtaquesDeReina(posicionReina, individuo, determinarSegmentoPertenece(segmentos, posicionReina));
+        // }
+        System.out.println("numero ataques:" + determinarNumeroAtaquesDeReina(0, individuo, segmentos));
     }
 
     public void seleccion() {
@@ -86,5 +96,154 @@ public class Algoritmo {
             
         }
         return listaSegmentos;
+    }
+
+    private int[] determinarSegmentoPertenece(List<int[]> segmentos, int posicionReina){
+        for (int[] segmento : segmentos) {
+            if ((posicionReina >= segmento[0]) && (posicionReina <= segmento[1])) {
+                return segmento;
+            }
+        }
+        return null;
+    }
+
+    private int indentificarFinEsquinaInferiorIzquierda(List<int[]> segmentos, int posicionReina, int[] individuo){
+        int actual=posicionReina, despues=0;
+        while ((actual + 7) <= 63) {
+            despues = actual + 7;
+            int[] s1 = determinarSegmentoPertenece(segmentos, actual);
+            int[] s2 = determinarSegmentoPertenece(segmentos, despues);
+
+            if (s1 != null && s2 != null) {
+                if (s1 == s2 || ((s1[1]+1) != s2[0])) {
+                    break;
+                }
+            }
+            actual += 7;
+        }
+        return actual;
+    }
+
+    private int indentificarFinEsquinaInferiorDerecha(List<int[]> segmentos, int posicionReina, int[] individuo){
+        int actual=posicionReina, despues=0;
+        while ((actual + 9) <= 63) {
+            despues = actual + 9;
+            int[] s1 = determinarSegmentoPertenece(segmentos, actual);
+            int[] s2 = determinarSegmentoPertenece(segmentos, despues);
+
+            if (s1 != null && s2 != null) {
+                if ( (s1 == s2) || ((s1[1]+1) != s2[0]) ) {
+                    break;
+                }
+            }
+            actual += 9;
+        }
+        return actual;
+    }
+
+    private int indentificarFinEsquinaSuperiorDerecha(List<int[]> segmentos, int posicionReina, int[] individuo){
+        int actual=posicionReina, despues=0;
+        while ((actual - 7) >= 0) {
+            despues = actual - 7;
+            int[] s1 = determinarSegmentoPertenece(segmentos, actual);
+            int[] s2 = determinarSegmentoPertenece(segmentos, despues);
+
+            if (s1 != null && s2 != null) {
+                if (s1 == s2 || ((s2[1]+1) != s1[0])) {
+                    break;
+                }
+            }
+            actual -= 7;
+        }
+        return actual;
+    }
+
+    private int indentificarFinEsquinaSuperiorIzquierda(List<int[]> segmentos, int posicionReina, int[] individuo){
+        int actual=posicionReina, despues=0;
+        while ((actual - 9) >= 0) {
+            despues = actual - 9;
+            int[] s1 = determinarSegmentoPertenece(segmentos, actual);
+            int[] s2 = determinarSegmentoPertenece(segmentos, despues);
+
+            if (s1 != null && s2 != null) {
+                if (s1 == s2 || ((s2[1]+1) != s1[0])) {
+                    break;
+                }
+            }
+            actual -= 9;
+        }
+        return actual;
+    }
+
+    private int determinarNumeroAtaquesDeReina(int posicionReina, int[] individuo, List<int[]> segmentos) {
+        int numeroAtaques = 0;
+        int[] segmentosPertenece = determinarSegmentoPertenece(segmentos, posicionReina);
+        //horizontal
+        for (int i = segmentosPertenece[0]; i < segmentosPertenece[1]; i++) {
+            if ((i != posicionReina) && (individuo[i] == 1)) {
+                numeroAtaques++;
+            }
+        }
+
+        //vertical arriba
+        int aux = posicionReina;
+        while ((aux - 8) >= 0) {
+            if (individuo[(aux - 8)] == 1) {
+                numeroAtaques++;
+            }
+            aux -= 8;
+        }
+        //vertical abajo
+        aux = posicionReina;
+        while ((aux + 8) <= 63) {
+            if (individuo[(aux + 8)] == 1) {
+                numeroAtaques++;
+            }
+            aux += 8;
+        }
+
+        //diagonal1 abajo
+        aux = posicionReina;
+        while ((aux + 7) <= indentificarFinEsquinaInferiorIzquierda(segmentos, aux, individuo)) {
+            if (individuo[(aux + 7)] == 1) {
+                numeroAtaques++;
+            }
+            aux += 7;  
+            System.out.println(aux);
+
+        }
+        //diagonal1 arriba
+        aux = posicionReina;
+        while ((aux - 7) >= indentificarFinEsquinaSuperiorDerecha(segmentos, aux, individuo)) {
+            if (individuo[(aux - 7)] == 1) {
+                numeroAtaques++;
+            }
+            aux -= 7;
+            System.out.println(aux);
+
+        }
+
+        //diagonal2 arriba
+        aux = posicionReina;
+        while ((aux - 9) >= indentificarFinEsquinaSuperiorIzquierda(segmentos, aux, individuo)) {
+            if (individuo[(aux - 9)] == 1) {
+                numeroAtaques++;
+            }
+            aux -= 9;
+            System.out.println(aux);
+
+        }
+
+        //diagonal2 abajo
+        aux = posicionReina;
+        while ((aux + 9) <= indentificarFinEsquinaInferiorDerecha(segmentos, aux, individuo)) {
+            if (individuo[(aux + 9)] == 1) {
+                numeroAtaques++;
+            }
+            aux += 9;
+            System.out.println(aux);
+        }
+
+        return numeroAtaques;
     }
 }
